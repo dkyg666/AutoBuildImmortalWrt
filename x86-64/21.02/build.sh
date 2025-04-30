6f7377f75b237ee75b0aea8e797d9d95038342d5
dkyg666 <43946866+dkyg666@users.noreply.github.com>
2025-04-30 21:24:45 +0800

    Update build.sh

--- x86-64/21.02/build.sh
+++ x86-64/21.02/build.sh
@@ -1,1 +1,67 @@
+#!/bin/bash
+# Log file for debugging
+LOGFILE="/tmp/uci-defaults-log.txt"
+echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
+echo "编译固件大小为: $PROFILE MB"
+echo "Include Docker: $INCLUDE_DOCKER"
 
+echo "Create pppoe-settings"
+mkdir -p  /home/build/immortalwrt/files/etc/config
+
+# 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
+cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
+enable_pppoe=${ENABLE_PPPOE}
+pppoe_account=${PPPOE_ACCOUNT}
+pppoe_password=${PPPOE_PASSWORD}
+EOF
+
+echo "cat pppoe-settings"
+cat /home/build/immortalwrt/files/etc/config/pppoe-settings
+# 输出调试信息
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
+
+
+
+PACKAGES=""
+PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES cfdisk"
+PACKAGES="$PACKAGES lsblk"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-turboacc-zh-cn"
+PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-sqm-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-smartdns-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-netdata-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ssr-plus-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-beardropper-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
+
+# 判断是否需要编译 Docker 插件
+if [ "$INCLUDE_DOCKER" = "yes" ]; then
+    PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+    echo "Adding package: luci-i18n-dockerman-zh-cn"
+fi
+
+# 构建镜像
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
+echo "$PACKAGES"
+
+make image PROFILE="generic" PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$PROFILE
+
+if [ $? -ne 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
+    exit 1
+fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."
